fb343202d1ae8b65e7633d44e735a2b3ef7c2cf4
Dominik <dmnsre@gmail.com>
2020-12-14 22:48:18 +0100

    refactor

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import { URL, API_KEY, PAGE_SIZE } from './constants';
 
 export const fetchFood = async (page, query) => {
   try {
-    const res = await fetch(`${URL}?query=${query}&api_key=${API_KEY}&pageSize=${PAGE_SIZE}&pageNumber=${page}`);
+    const res = await fetch(
+      `${URL}?query=${query}&api_key=${API_KEY}&pageSize=${PAGE_SIZE}&pageNumber=${page}`
+    );
     const data = await res.json();
 
     return data;
